ba95dd27e97ad4340ef450f754147458d076f66c
BaljotSingh001 <baljotsingh001@algomau.ca>
2024-06-27 23:01:19 -0400

    Add files via upload

---
+++ Week-Seven/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class = "footerTest">
+
+    </footer>
+</body>
+</html>
---
+++ Week-Seven/php/newpage.php
@@ -0,0 +1,29 @@
+<style>
+
+#shere{
+    text-align: center;
+    font-style: oblique;
+    color: rosybrown;
+    
+}
+#behaveTest{
+    text-align: center;
+    font-size: small;
+}
+
+
+h1{
+    text-align: center;
+    
+    padding: 10px;
+    
+}
+
+
+ </style>
+<?php include '../includes/header.php' ?>
+<body id="behavebody">
+
+</body>
+
+<?php include '../includes/footer.php' ?> 
